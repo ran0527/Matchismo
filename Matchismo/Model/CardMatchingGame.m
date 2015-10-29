3babbb19f3554f3a67511ca13a7adc42d63d3ca5
Xinran <lindashi527@gmail.com>
2015-10-29 12:20:25 -0700

    fix bug

--- Matchismo/Model/CardMatchingGame.m
+++ Matchismo/Model/CardMatchingGame.m
@@ -65,8 +65,8 @@ static const int COST_TO_CHOOSE = 1;
                         self.score -= MISMATCH_PENALTY;
                         otherCard.chosen = NO;
                     }
+                    break;
                 }
-                break;
             }
             self.score -= COST_TO_CHOOSE;
             card.chosen = YES;
